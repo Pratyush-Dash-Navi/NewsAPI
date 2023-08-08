e2d027b728a732c38ebe7ded3cf1562b18996631
pratyush <pratyush.dash@navi.com>
2023-08-08 10:57:36 +0530

    First stretch goal completed

--- src/main/java/com/navi/NewsAPI/controller/NewsAPIController.java
+++ src/main/java/com/navi/NewsAPI/controller/NewsAPIController.java
@@ -4,6 +4,7 @@ import com.navi.NewsAPI.service.impl.NewsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequestMapping("/news")
@@ -24,6 +25,11 @@ public class NewsAPIController {
     @GetMapping(value = "/getCountries")
     public List<String> countries(){
         return newsImpl.printCountries();
+    }
+
+    @GetMapping(value = "/readCSV")
+    public List<String[]> readCountries() throws IOException {
+        return newsImpl.readDataFromCSV();
     }
 
 }
--- src/main/java/com/navi/NewsAPI/service/impl/NewsImpl.java
+++ src/main/java/com/navi/NewsAPI/service/impl/NewsImpl.java
@@ -1,8 +1,15 @@
 package com.navi.NewsAPI.service.impl;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 @Component
@@ -10,6 +17,39 @@ public class NewsImpl {
     List<String> categories = Arrays.asList("business","entertainment","general");
     List<String> countries = Arrays.asList("us","ar","rs","hu");
 
+    private static final String FILE_PATH = "/Users/pratyushdash/Downloads/NewsAPI/src/main/resources/lists.csv";
+    public static List<String[]> readDataFromCSV() throws IOException {
+        List<String[]> data = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReaderBuilder(new FileReader(FILE_PATH)).build()) {
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                data.add(line);
+            }
+        } catch (CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+
+        return data;
+    }
+
+    // Method to write data to CSV file
+    public static void writeDataToCSV(List<String[]> data) throws IOException {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(FILE_PATH))) {
+            writer.writeAll(data);
+        }
+    }
+
+//    public List<String> readFromCSV() {
+//        List<String> countries = new ArrayList<>();
+//        try(FileWriter writer = new FileWriter("/Users/pratyushdash/Downloads/NewsAPI/src/main/resources/lists.csv")){
+//            for()
+//        }
+//        catch (IOException e){
+//            System.out.println("IOE");
+//            e.printStackTrace();
+//        }
+//    }
 
     public List<String> printCategories(){
         for(String s : categories){
