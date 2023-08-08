3db6e05453c53d4ff1914606deeefe241e9ea3b5
pratyush <pratyush.dash@navi.com>
2023-08-08 11:31:42 +0530

    JUnits added

---
+++ src/test/java/com/navi/NewsAPI/controller/NewsAPIControllerTest.java
@@ -0,0 +1,48 @@
+package com.navi.NewsAPI.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewsAPIControllerTest {
+
+    @Test
+    void print() {
+    }
+
+    @Test
+    void categories() {
+    }
+
+    @Test
+    void countries() {
+    }
+
+    @Test
+    void readCountries() {
+    }
+
+    @Test
+    void editCountries() {
+    }
+
+    @Test
+    void testPrint() {
+    }
+
+    @Test
+    void testCategories() {
+    }
+
+    @Test
+    void testCountries() {
+    }
+
+    @Test
+    void testReadCountries() {
+    }
+
+    @Test
+    void testEditCountries() {
+    }
+}
---
+++ src/test/java/com/navi/NewsAPI/service/impl/NewsImplTest.java
@@ -0,0 +1,24 @@
+package com.navi.NewsAPI.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewsImplTest {
+
+    @Test
+    void readDataFromCSV() {
+    }
+
+    @Test
+    void editDataInCSV() {
+    }
+
+    @Test
+    void printCategories() {
+    }
+
+    @Test
+    void printCountries() {
+    }
+}
